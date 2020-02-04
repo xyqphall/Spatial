6ae545258dd88e20bff35d8f60b3b6ca48d7238c
Samuel Ytterbrink <neppord@gmail.com>
2020-02-04 20:16:32 +0100

    Add background to game.

--- src/Main.elm
+++ src/Main.elm
@@ -1,7 +1,46 @@
 module Main exposing (main)
 
-import Html exposing (text)
+import Browser exposing (element)
+import Html.Attributes exposing (style)
+import TypedSvg exposing (svg)
+import TypedSvg.Attributes exposing (height, viewBox, width)
+import TypedSvg.Types exposing (num)
+
+
+screenWidth =
+    1920
+
+
+screenHeight =
+    1200
 
 
 main =
-    text "Hello World!"
+    element
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
+
+
+init () =
+    ( (), Cmd.none )
+
+
+view () =
+    svg
+        [ width <| num screenWidth
+        , height <| num screenHeight
+        , viewBox 0 0 screenWidth screenHeight
+        , style "background" "url(/images/BG_Main.png)"
+        ]
+        []
+
+
+update () () =
+    ( (), Cmd.none )
+
+
+subscriptions () =
+    Sub.none
